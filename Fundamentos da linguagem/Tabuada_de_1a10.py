2c93da23ea5681c4561b9098c062d9b132f045af
Enzo <enzogabriel8515@gmail.com>
2025-11-21 11:29:28 -0300

    Tabuada simples

--- Fundamentos da linguagem/Tabuada_de_1a10.py
+++ Fundamentos da linguagem/Tabuada_de_1a10.py
@@ -1,5 +1,5 @@
 num = int(input("Informe um número para obter a tabuada de 1 a 10: "))
-print(f"\nTabuada de {num}:\n")
+print(f" Tabuada de {num}:")
 for i in range (1,11):
   multp = num*i
   print(f"{num} x {i} = {multp}\n") 
