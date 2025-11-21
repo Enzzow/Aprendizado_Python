07f7106fab46691e457cb793bc766f16cf12e5a0
Enzo <enzogabriel8515@gmail.com>
2025-11-21 10:56:47 -0300

    Organizando exercícios na pasta Fundamentos da linguagem

--- Tabuada_de_1a10.py
+++
@@ -1,6 +0,0 @@
-num = int(input("Informe um número para obter a tabuada de 1 a 10: "))
-print(f"\nTabuada de {num}:\n")
-for i in range (1,11):
-  multp = num*i
-  print(f"{num} x {i} = {multp}\n") 
-  
