10dbf52ff2c6e4806df7181c116485c178883542
Enzo <enzogabriel8515@gmail.com>
2025-11-22 15:32:37 -0300

    Desafio do Palíndromo
    Palíndromo são palavras ,frases ou números que mesmo tendo a ordem invertida , permanece igual ao original.

--- Fundamentos da linguagem/palindromo.py
+++ Fundamentos da linguagem/palindromo.py
@@ -6,4 +6,6 @@ if (string == string_invertida):
     print(" É um palíndromo")
 else:
     print(" Não é um palíndromo")
+
+#A diferença de construir a lógica para verificar um palíndromo em Python para C é mt grande kkkkkj
     
