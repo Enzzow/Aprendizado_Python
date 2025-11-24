e2e8cda06a5d38e61089e25d3a606d002cf550eb
Enzo <enzogabriel8515@gmail.com>
2025-11-24 09:13:49 -0300

    Verifica se o aluno foi aprovado ou reprovado

---
+++ Fundamentos da linguagem/Estruturas condicionais/notas.py
@@ -0,0 +1,15 @@
+NUM 2
+
+soma = 0
+nome = input(" Informe o nome do aluno: ")
+               
+for i in range(NUM):
+  nota = int(input(f" Informe a {i+1}º nota: "))
+  soma += nota
+
+if (soma/2)>= 7:
+  print(f" Aluno(a) {nome} foi Aprovado(a)!")
+else:
+  print(f" Aluno(a) {nome} Reprovado(a)!")
+
+
