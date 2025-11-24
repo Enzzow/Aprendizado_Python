87f4d60981de23d6e02bfe98a417d862a4753759
Enzo <enzogabriel8515@gmail.com>
2025-11-24 09:17:51 -0300

    Calcula a média e define se o aluno foi aprovado ou reprovado.

--- Fundamentos da linguagem/Estruturas condicionais/notas.py
+++ Fundamentos da linguagem/Estruturas condicionais/notas.py
@@ -7,7 +7,7 @@ for i in range(NUM):
   nota = int(input(f" Informe a {i+1}º nota: "))
   soma += nota
 
-if (soma/2)>= 7:
+if (soma/NUM)>= 7:
   print(f" Aluno(a) {nome} foi Aprovado(a)!")
 else:
   print(f" Aluno(a) {nome} Reprovado(a)!")
